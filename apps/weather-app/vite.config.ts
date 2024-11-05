eee60682f981c274a373da9e1a6d03f4b8ef6829
Bill Story <storywr@gmail.com>
2024-11-04 19:54:59 -0500

    add api url env var

--- apps/weather-app/vite.config.ts
+++ apps/weather-app/vite.config.ts
@@ -1,35 +1,39 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
 
-export default defineConfig({
-  root: __dirname,
-  cacheDir: '../../node_modules/.vite/apps/weather-app',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // Replace with your NestJS port
-        changeOrigin: true,
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    root: __dirname,
+    cacheDir: '../../node_modules/.vite/apps/weather-app',
+    server: {
+      proxy: {
+        '/api': {
+          target: `${env.VITE_API_URL}`,
+          changeOrigin: true,
+        },
       },
     },
-  },
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-  plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-  build: {
-    outDir: '../../dist/apps/weather-app',
-    emptyOutDir: true,
-    reportCompressedSize: true,
-    commonjsOptions: {
-      transformMixedEsModules: true,
+    preview: {
+      port: 4300,
+      host: 'localhost',
     },
-  },
+    plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    build: {
+      outDir: '../../dist/apps/weather-app',
+      emptyOutDir: true,
+      reportCompressedSize: true,
+      commonjsOptions: {
+        transformMixedEsModules: true,
+      },
+    },
+  };
 });
