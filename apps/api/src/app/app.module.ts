84bdbcc3fb201f570e9767fb50b0955639d5c01c
Bill Story <storywr@gmail.com>
2024-11-04 23:29:48 -0500

    cleanup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,14 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController, WeatherController],
-  providers: [AppService, WeatherService],
+  controllers: [WeatherController],
+  providers: [WeatherService],
 })
 export class AppModule {}
