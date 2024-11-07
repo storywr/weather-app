d92727b0c62c8d861ab2a44b8dd652ff7a468ad9
Bill Story <storywr@gmail.com>
2024-11-06 22:24:02 -0600

    cleanup weather service

--- apps/api/src/weather/weather.service.ts
+++ apps/api/src/weather/weather.service.ts
@@ -6,53 +6,40 @@ import { ConfigService } from '@nestjs/config';
 export class WeatherService {
   constructor(private configService: ConfigService) {}
 
+  private appId = this.configService.get<string>('VITE_WEATHER_KEY');
+
+  private fetchData = async (urlSuffix: string) => {
+    const baseURL = 'https://api.openweathermap.org/';
+    const response = await fetch(`${baseURL}${urlSuffix}`);
+    const data = response.json();
+    return data;
+  };
+
   async getCoordinates(search): Promise<{ data: City }> {
     const zipRegex = /^\d{5}(?:-\d{4})?$/;
     const isZip = zipRegex.test(search);
-
-    let cityData;
-    if (isZip) {
-      const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/zip?zip=${search},US&limit=5&appid=${this.configService.get<string>(
-          'VITE_WEATHER_KEY'
-        )}`
-      );
-      cityData = await response.json();
-    } else {
-      const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${search},US&limit=5&appid=${this.configService.get<string>(
-          'VITE_WEATHER_KEY'
-        )}`
-      );
-      const cities = await response.json();
-      cityData = cities;
-    }
-
+    const cityData = await this.fetchData(
+      isZip
+        ? `geo/1.0/zip?zip=${search},US&limit=5&appid=${this.appId}`
+        : `geo/1.0/direct?q=${search},US&limit=5&appid=${this.appId}`
+    );
     return cityData;
   }
 
   async getWeather(lat: number, lon: number): Promise<{ data: WeatherData }> {
-    const weather = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${this.configService.get<string>(
-        'VITE_WEATHER_KEY'
-      )}`
+    const data = await this.fetchData(
+      `data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${this.appId}`
     );
-    const weatherData = await weather.json();
-
-    return weatherData;
+    return data;
   }
 
   async getForecast(
     lat: number,
     lon: number
   ): Promise<{ data: { list: WeatherData[] } }> {
-    const weather = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=40&appid=${this.configService.get<string>(
-        'VITE_WEATHER_KEY'
-      )}`
+    const data = await this.fetchData(
+      `data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&cnt=40&appid=${this.appId}`
     );
-    const weatherData = await weather.json();
-
-    return weatherData;
+    return data;
   }
 }
